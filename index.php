4fe000b3a15c07f5edea68bb3437dfc3364a7eac
Breno Brandao <breno_brandao12@hotmail.com>
2021-07-22 20:12:39 -0300

    adicionados arquivos iniciais

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tabuada</title>
+</head>
+<body>
+    <form action="tabuada.php"method="get">
+        <input type="number" name="num">
+        <button type="submit">enviar</button>
+    </form>
+</body>
+</html>
---
+++ tabuada.php
@@ -0,0 +1,16 @@
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabuada</title>
+</head>
+<body>
+</php for ($i = 1; $i <= 9; $i++): ?>
+    <h3><?php echo 'res: ' . $i * $num; ?> </h3>
+    <hr>
+    <?php endfor ?>
+
+</body>
+</html>
